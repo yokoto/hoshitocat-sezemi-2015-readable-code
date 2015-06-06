93c078f43c7cf826bcae88fc62ee2b2dedcdba0e
yokoto <youkotoushi@gmail.com>
2015-06-06 15:29:46 +0900

    仕様8 done

--- task.rb
+++ task.rb
@@ -1,31 +1,31 @@
+# ファイル名の入力
 print "ファイル名を入力して下さい(終了したいときは\"exit\"と入力) > "
 
 while filename = STDIN.gets
   break if filename.chomp! == "exit"
   puts "指定したファイル名 > #{filename}"
 
+  # レシピのIDの入力
   print "IDを入力して下さい(終了したいときは\"exit\"と入力) > "
   while recipe_id = STDIN.gets
-    recipe_id = recipe_id.to_i
-    puts "指定したID > #{recipe_id}" if recipe_id.to_i > 0
-    break
+	recipe_id = recipe_id.to_i
+	puts "指定したID > #{recipe_id}" if recipe_id.to_i > 0
+
+	file = File.new(filename)
+
+	recipes = []
+	file.each_line { |line| recipes << line }
+	recipe = recipes[recipe_id - 1]
+	recipe.nil? ? (puts "nothing") : (puts "#{recipe_id}: #{recipe}")
+
+	# ユーザー名の入力
+	print "名前を入力してください > "
+
+	while name = STDIN.gets
+	  puts "ユーザー名 > #{name}"
+	  break
+	end
+	break
   end
-
-  # recipe-dataのファイルを受け取る
-  file = File.new(filename)
-
-  recipes = []
-  file.each_line { |line| recipes << line }
-
-  if recipe_id > 0
-    recipe = recipes[recipe_id - 1]
-    recipe.nil? ? (puts "nothing") : (puts "#{recipe_id}: #{recipe}")
-    break
-  else
-    recipes.each_with_index do |recipe, id| 
-      puts "#{id + 1}: #{recipe}"
-    end
-  end
-
   break
 end
